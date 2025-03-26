8d45e31004644ed06f2e6b9591f7aba3340dd331
bebeybena <bebeybena@IUTRS6>
2025-03-26 11:20:41 +0100

    Ajoutez des fichiers projet.

---
+++ commandeClient/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace commandeClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbxNumCom = new System.Windows.Forms.TextBox();
+            this.tbxDest = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 14F);
+            this.label1.Location = new System.Drawing.Point(174, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numéro de commande :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 14F);
+            this.label2.Location = new System.Drawing.Point(174, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Client concerné :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 14F);
+            this.label3.Location = new System.Drawing.Point(174, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(205, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Employé responsable :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 14F);
+            this.label4.Location = new System.Drawing.Point(174, 249);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 22);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Date commande :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 14F);
+            this.label5.Location = new System.Drawing.Point(174, 360);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(120, 22);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Destinataire :";
+            // 
+            // tbxNumCom
+            // 
+            this.tbxNumCom.Location = new System.Drawing.Point(424, 61);
+            this.tbxNumCom.Name = "tbxNumCom";
+            this.tbxNumCom.Size = new System.Drawing.Size(210, 20);
+            this.tbxNumCom.TabIndex = 5;
+            // 
+            // tbxDest
+            // 
+            this.tbxDest.Location = new System.Drawing.Point(424, 364);
+            this.tbxDest.Name = "tbxDest";
+            this.tbxDest.Size = new System.Drawing.Size(210, 20);
+            this.tbxDest.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(424, 115);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(206, 21);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(424, 180);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(206, 21);
+            this.comboBox2.TabIndex = 8;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(424, 243);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 514);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.tbxDest);
+            this.Controls.Add(this.tbxNumCom);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbxNumCom;
+        private System.Windows.Forms.TextBox tbxDest;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+    }
+}
+
---
+++ commandeClient/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace commandeClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SQLiteConnection connec = new SQLiteConnection();
+        string chcon = @"Data source = Comptoir2.sqlite";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // Paramétrage de la connextion
+                connec.ConnectionString = chcon;
+                connec.Open();
+                MessageBox.Show(connec.State.ToString());
+
+                string requete = @"select N° commande from Commandes";
+                MessageBox.Show(requete);
+
+                SQLiteCommand cd = new SQLiteCommand();
+                cd.CommandType = CommandType.Text;
+                cd.CommandText = requete;
+            }
+            catch (Exception erreur)
+            {
+                MessageBox.Show(erreur.GetType().ToString());
+            }
+            finally { }
+        }
+    }
+}
